b1bce6aaab0e751bc28308f40ef7424fe97ed9f1
Tomislav Dukez <tomdu3@ymail.com>
2024-01-20 06:01:41 +0000

    Fix: separate js file for homepage

--- static/js/script.js
+++ static/js/script.js
@@ -5,23 +5,3 @@ $(document).ready(function () {
 		$(".alert-dismissible").fadeOut("slow");
 	}, 5000);
 });
-function setDisplayClass() {
-	const heading = document.getElementById("responsive-heading");
-
-	if (window.matchMedia("(min-width: 1200px)").matches) {
-		heading.classList.remove("display-5");
-		heading.classList.add("display-1");
-	} else if (window.matchMedia("(min-width: 768px)").matches) {
-		heading.classList.remove("display-1");
-		heading.classList.add("display-5");
-	} else {
-		heading.classList.remove("display-5");
-		heading.classList.add("display-1");
-	}
-}
-
-// Initial call to set the display class on page load
-setDisplayClass();
-
-// Event listener to update the display class when the window is resized
-window.addEventListener("resize", setDisplayClass);
