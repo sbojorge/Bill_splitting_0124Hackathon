dfc80848c106621f3eb5c9232f5292388f351f07
sbojorge <sara.bojorge@gmail.com>
2024-01-21 07:31:54 +0100

    Revert "Merge pull request #78 from sbojorge/aldwin-dev"
    This reverts commit 17ed65689304bf4fd2838a3f127844bddc9b06f3, reversing
    changes made to 3aaa9a881e99567ef3667c5b485ab9f8eeef5462.

--- main/urls.py
+++ main/urls.py
@@ -3,7 +3,7 @@ from .views import HomeView, EventView, DisplayEvent, ExpenseView
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
-    path('', EventView.as_view(), name='new_event'),
+    path('event/', EventView.as_view(), name='new_event'),
     path('index/',DisplayEvent.as_view(), name='index'),
     path('expense/<int:event_id>/', ExpenseView.as_view(), name='expenseCalculatePage'),
 ]
