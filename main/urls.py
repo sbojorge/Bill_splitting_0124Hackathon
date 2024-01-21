d9ba40801a64308bda5d25e09fa56a70b9040e22
MuzhdaN <muzhda.noorzad4@gmail.com>
2024-01-20 21:18:54 -0600

    Add views for expenses and functionality to display members dynamically

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import HomeView, EventView
+from .views import HomeView, EventView, ExpenseView
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('event/', EventView.as_view(), name='new_event'),
+    path('expense/<int:event_id>/', ExpenseView.as_view(), name='expenseCalculatePage'),
 ]
