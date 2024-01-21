baf727837faa7f49931830485d82235b6bb81471
Aldwin Arriola <araldwin@gmail.com>
2024-01-21 05:58:51 +0100

    modify, add style to new-event.html, create a modal for create event, remove url pathevent/, modify alerts and validation for UI/UX design, responsive design

--- main/urls.py
+++ main/urls.py
@@ -3,10 +3,6 @@ from .views import HomeView, EventView, DisplayEvent
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
-<<<<<<< Updated upstream
-    path('event/', EventView.as_view(), name='new_event'),
-    path('index/',DisplayEvent.as_view(), name='index'),
-=======
     path('', EventView.as_view(), name='new_event'),
->>>>>>> Stashed changes
+    path('index/',DisplayEvent.as_view(), name='index'),
 ]
