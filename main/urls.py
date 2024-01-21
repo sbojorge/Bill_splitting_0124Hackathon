b756cbeb0fe4b22d7b60a183244b1a972758dd9f
sbojorge <sara.bojorge@gmail.com>
2024-01-21 08:14:07 +0100

    Fix reverting code before Aldwin's changes

--- main/urls.py
+++ main/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
-from .views import HomeView, EventView, DisplayEvent, ExpenseView
+from .views import HomeView, EventView, DisplayEvent
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
-    path('', EventView.as_view(), name='new_event'),
+    path('event/', EventView.as_view(), name='new_event'),
     path('index/',DisplayEvent.as_view(), name='index'),
-    path('expense/<int:event_id>/', ExpenseView.as_view(), name='expenseCalculatePage'),
 ]
