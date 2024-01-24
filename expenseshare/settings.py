82077d99dbf07541c59fc8178bb54367e41c927e
sbojorge <sara.bojorge@gmail.com>
2024-01-24 17:12:58 +0100

    Remove unnecessary media directory and media vars for Cloudinary in settings.py

--- expenseshare/settings.py
+++ expenseshare/settings.py
@@ -157,13 +157,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
 STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-MEDIA_URL = '/media/'
-DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+
 CLOUDINARY_URL = os.environ.get('CLOUDINARY_URL')
 
 
